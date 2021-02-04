7f531d221eabe6dc457925711271a2908ffaf173
Christopher Hicks <visrule@protonmail.com>
2021-02-03 18:11:22 -0800

    Initial Commit

---
+++ Calorie/CalorieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieApp.swift
+//  Calorie
+//
+//  Created by Christopher Hicks on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CalorieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
